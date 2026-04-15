714c12da23598281313b8edf5e69baf48590c788
Aditya Suryawanshi <adityassuryawanshi25@gmail.com>
2026-04-15 19:40:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,3 @@ st.write("Upload your PDF here.")
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 if uploaded_file is not None:
     st.write("PDF uploaded successfully!")
-
-else:
-  st.write("PDF not detected")
